c7d22a53a6810e2b07d964afa8a1f28b8db2944f
hitfuture <brett.r.taylor@me.com>
2018-07-24 15:47:00 +0000

    undo import("pandas", as="pd") in testing so that we can see if this is causing the problem

--- tests/testthat/test-function-renames.R
+++ tests/testthat/test-function-renames.R
@@ -2,11 +2,11 @@ context("vectors")
 
 test_that("Single element vectors are treated as scalars", {
   skip_if_no_python()
-  pd <- import("pandas",as = "pd")
-
-  df <- iris
-  df <- r_to_py(df)
-
-  df <- py_to_r(df)
-  expect_is(df,"data.frame")
+  # pd <- import("pandas",as = "pd")
+  #
+  # df <- iris
+  # df <- r_to_py(df)
+  #
+  # df <- py_to_r(df)
+  # expect_is(df,"data.frame")
 })
